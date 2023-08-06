01ace672f66e228f81971298538e0190bbfd1572
husamhammad <husamhammad542@gmail.com>
2023-08-06 16:08:56 +0300

    refactor : apply new edit

--- email_reminder/utils/reminder.py
+++ email_reminder/utils/reminder.py
@@ -1,51 +1,54 @@
 import frappe
 import json
-
+from frappe import __
+import re
 
 @frappe.whitelist()
-def get_doctypes():
-    docs = frappe.db.sql("""SELECT * FROM `tabReminder Doctypes`""", as_dict=1)
-    return [doc.document_type for doc in docs]
+def fetch_reminder_doctypes():
+    reminder_doctypes = frappe.db.sql("""SELECT * FROM `tabReminder Doctypes`""", as_dict=1)
+    return [doc.document_type for doc in reminder_doctypes]
 
 
 @frappe.whitelist()
 def send_email(message, recipients, reminder_schedule_date, doctype, docname, site):
     try:
         data = json.loads(recipients)
-        emails_ = [email["email"] for email in data if email]
-        if not emails_:
-            frappe.throw("Please add at least one email")
+        emails = [email["email"] for email in data if email]
+        if not emails:
+            frappe.throw(__("Please add at least one email"))
+            
+        message = f"""{doctype}<br>" Reminder on Document {docname}<br><br>"{message}<br><br>"
+                <a href='{site}/app/{doctype.lower().replace(' ', '-')}/{docname}'>Open Document</a>"""
 
-        message = doctype + "<br>" + "Reminder on Document " + docname
-        +"<br><br>" + message + "<br><br> <a href='" + site + "/app/"
-        +doctype.lower().replace(" ", "-")
-        + "/" + docname + "' >Open Document</a>"
-
-        create_reminder(message, emails_, doctype, docname)
+        create_reminder(message, emails, doctype, docname)
 
         frappe.sendmail(
-            recipients=emails_,
+            recipients=emails,
             subject="Details",
             message=message,
             send_after=reminder_schedule_date,
         )
 
-        return "Success"
-
-    except:
-        frappe.log_error(frappe.get_traceback())
-        return "Failed"
+        return __("Success")
+    
+    except json.JSONDecodeError:
+        frappe.log_error(f"JSON decoding error: {frappe.get_traceback()}")
+        return __("Invalid JSON format , Check It and Try Again")
+    
+    except Exception as e:
+        frappe.log_error(f"An Error Occurred: {e}\n{frappe.get_traceback()}")
+        return __("Failed")
 
 
 @frappe.whitelist()
-def create_reminder(message, emails_, doctype, docname):
+def create_reminder(message, emails, doctype, docname):
     reminder = frappe.get_doc(
         {
             "doctype": "Reminder",
             "message": message,
             "document_type": doctype,
             "document_no": docname,
-            "emails": get_emails(emails_),
+            "emails": get_emails(emails),
         }
     )
     reminder.flags.ignore_permissions = True
@@ -53,10 +56,10 @@ def create_reminder(message, emails_, doctype, docname):
     reminder.submit()
 
 
-def get_emails(emails_):
-    emails = []
-    for email in emails_:
-        if "@" not in email:
-            frappe.throw("Invalid Email, Please Check It ! " + email)
-        emails.append({"email": email})
-    return emails
+def get_emails(emails):
+    emails_list = []
+    for email in emails:
+        if not re.match(r"[^@]+@[^@]+\.[^@]+", email):
+            frappe.throw(__(f"Invalid Email, Please Check It ! {email}"))
+        emails_list.append({"email": email})
+    return emails_list
