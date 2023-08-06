cba461262685ff1cf30d8bb17908c1abc17a855d
husamhammad <husamhammad542@gmail.com>
2023-08-06 14:42:23 +0300

    Refactor: Improve function parameter naming and code structure

--- email_reminder/utils/reminder.py
+++ email_reminder/utils/reminder.py
@@ -1,56 +1,62 @@
-import frappe, json
+import frappe
+import json
 
 
 @frappe.whitelist()
 def get_doctypes():
-    docs = frappe.db.sql(""" SELECT * FROM `tabReminder Doctypes` """, as_dict=1)
-
-    return [i.document_type for i in docs]
+    docs = frappe.db.sql("""SELECT * FROM `tabReminder Doctypes`""", as_dict=1)
+    return [doc.document_type for doc in docs]
 
 
 @frappe.whitelist()
 def send_email(message, recipients, reminder_schedule_date, doctype, docname, site):
     try:
         data = json.loads(recipients)
-        emails_ = [i['email'] for i in data if i]
+        emails_ = [email["email"] for email in data if email]
         if not emails_:
-            frappe.throw("Please add emails")
-        f_message = doctype + "<br>" + "Reminder on Document " + docname + "<br><br>" + message + "<br><br> <a href='" + site + "/app/" + doctype.lower().replace(
-            " ", "-") + "/" + docname + "' >Open Document</a>"
+            frappe.throw("Please add at least one email")
+
+        message = doctype + "<br>" + "Reminder on Document " + docname
+        +"<br><br>" + message + "<br><br> <a href='" + site + "/app/"
+        +doctype.lower().replace(" ", "-")
+        + "/" + docname + "' >Open Document</a>"
+
         create_reminder(message, emails_, doctype, docname)
-        print(f_message)
+
         frappe.sendmail(
             recipients=emails_,
             subject="Details",
-            message=f_message,
-            send_after=reminder_schedule_date
+            message=message,
+            send_after=reminder_schedule_date,
         )
-        return "Success"
-    except:
 
+        return "Success"
+
+    except:
         frappe.log_error(frappe.get_traceback())
         return "Failed"
 
 
 @frappe.whitelist()
 def create_reminder(message, emails_, doctype, docname):
-    obj = {
-        "doctype": "Reminder",
-        "message": message,
-        "document_type": doctype,
-        "document_no": docname,
-        "emails": get_emails(emails_)
-    }
-    rem = frappe.get_doc(obj)
-    rem.flags.ignore_permissions = True
-    rem.insert()    
-    rem.submit()
+    reminder = frappe.get_doc(
+        {
+            "doctype": "Reminder",
+            "message": message,
+            "document_type": doctype,
+            "document_no": docname,
+            "emails": get_emails(emails_),
+        }
+    )
+    reminder.flags.ignore_permissions = True
+    reminder.insert()
+    reminder.submit()
 
 
 def get_emails(emails_):
     emails = []
-
-    for i in emails_:
-        if "@" not in i:
-            frappe.throw("Invalid Email " + i)
-        emails.append({"email": i})
+    for email in emails_:
+        if "@" not in email:
+            frappe.throw("Invalid Email, Please Check It ! " + email)
+        emails.append({"email": email})
+    return emails
