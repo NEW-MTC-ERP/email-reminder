40caed1999a815f3e6601240fa57fccedb7c7cbf
husamhammad <husamhammad542@gmail.com>
2023-08-06 15:05:40 +0300

    Refactor:code structure and functions

--- email_reminder/public/js/test.js
+++ email_reminder/public/js/test.js
@@ -1,5 +1,3 @@
-var field_clicked = false
-
 $(document).on('app_ready', function() {
 	frappe.call({
 		method: "email_reminder.utils.reminder.fetch_reminder_doctypes",
@@ -11,25 +9,19 @@ $(document).on('app_ready', function() {
 							method: "email_reminder.utils.reminder.send_email",
 							args: {
 								message: cur_frm.doc.message,
-								recipients: cur_frm.doc.emails ? cur_frm.doc.emails : [],
-								reminder_schedule_date: cur_frm.doc.date_and_time ? cur_frm.doc.date_and_time : "" ,
+								recipients: cur_frm.doc.emails || [],
+								reminder_schedule_date: cur_frm.doc.date_and_time || "" ,
 								doctype: cur_frm.doc.doctype,
 								docname: cur_frm.doc.name,
 								site: window.origin
 							},
 							async: false,
 							callback: function (r) {
-								if(r.message === 'Success'){
-									 frappe.show_alert({
-												message:__('Email added to Email Queue'),
-												indicator:'green'
-											}, 5);
-								} else {
-									frappe.show_alert({
-												message:__('Error in sending email. Check Error Log or contact technical team'),
-												indicator:'red'
-											}, 5);
-								}
+								var message = r.message === 'Success'
+									? { message: __('Email added to Email Queue'), indicator: 'green' }
+									: { message: __('Error in sending email, Check Error Log or contact technical team'), indicator: 'red' };
+					
+								frappe.show_alert(message, 5);
 							}
 						})
 				});
@@ -51,56 +43,42 @@ function send_email(cur_frm) {
 			docname: cur_frm.doc.name,
 		},
 		async: false,
-		callback: function (r) {
-			if(r.message === 'Success'){
-				 frappe.show_alert({
-                            message:__('Email added to Email Queue'),
-                            indicator:'green'
-                        }, 5);
-			} else {
-				frappe.show_alert({
-                            message:__('Error in sending email. Check Error Log or contact technical team'),
-                            indicator:'red'
-                        }, 5);
-			}
+		callback: function(r) {
+            var message = r.message === 'Success'
+                ? { message: __('Email added to Email Queue'), indicator: 'green' }
+                : { message: __('Error in sending email, Check Error Log or contact technical team'), indicator: 'red' };
+
+            frappe.show_alert(message, 5);
         }
 	})
 }
 function filterFunction() {
-  var input, filter, ul, li, a, i;
-  input = document.getElementById("myInput");
-  filter = input.value.toUpperCase();
-  var div = document.getElementById("myDropdown");
-  a = div.getElementsByTagName("a");
+	var input = document.getElementById("myInput");
+    var filter = input.value.toUpperCase();
+    var div = document.getElementById("myDropdown");
+    var a = div.getElementsByTagName("a");
+
   for (i = 0; i < a.length; i++) {
     var txtValue = a[i].textContent || a[i].innerText;
-    if (txtValue.toUpperCase().indexOf(filter) > -1) {
-      a[i].style.display = "";
-    } else {
-      a[i].style.display = "none";
-    }
+            a[i].style.display = txtValue.toUpperCase().indexOf(filter) > -1 ? "" : "none";
   }
+}
+
+function toggleDropdown(display) {
+    var a = document.querySelectorAll("#myDropdown a");
+
+    a.forEach(function(link) {
+        link.style.display = display;
+    });
 }
 
 function focusIn() {
-console.log("test onclick focus in")
-
-	field_clicked = true
- 	var a, i;
-	  var div = document.getElementById("myDropdown");
-	  a = div.getElementsByTagName("a");
-	  for (i = 0; i < a.length; i++) {
-			a[i].style.display = "";
-	  }
-
+    console.log("test onclick focus in");
+    field_clicked = true;
+    toggleDropdown("");
 }
-function focusOut() {
-	 var a, i;
-  var div = document.getElementById("myDropdown");
-  a = div.getElementsByTagName("a");
-  for (i = 0; i < a.length; i++) {
-  		a[i].style.display = "None";
-  }
 
+function focusOut() {
+    toggleDropdown("none");
 }
 
