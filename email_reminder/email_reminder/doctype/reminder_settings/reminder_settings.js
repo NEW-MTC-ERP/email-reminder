1ab1ba8786f740a9e9f3cffaa94e7e5017d93ba6
husamhammad <husamhammad542@gmail.com>
2023-08-08 01:34:08 +0300

    feat: Rebuild the code structure to achieve all functions

--- email_reminder/email_reminder/doctype/reminder_settings/reminder_settings.js
+++ email_reminder/email_reminder/doctype/reminder_settings/reminder_settings.js
@@ -2,11 +2,17 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Reminder Settings', {
+    refresh:function(frm){
+        cur_frm.disable_save()
+    },
 	generate_fields: function(frm) {
         cur_frm.call({
             doc: cur_frm.doc,
             method: "generate_fields",
             async: false,
+            args: {
+                docs:frm.doc.doctypes.filter(row => row.document_type && row.document_type != "").map(row => row.document_type)
+            } ,
             callback: function () {
                  frappe.show_alert({
                     message:__('Fields Created'),
@@ -16,3 +22,9 @@ frappe.ui.form.on('Reminder Settings', {
         })
 	}
 });
+
+// frappe.ui.form.on('Reminder Doctypes', {
+//     doctypes_remove:function(frm){
+//         cur_frm.trigger("generate_fields")
+//     },
+// });
--- email_reminder/email_reminder/doctype/reminder_settings/reminder_settings.py
+++ email_reminder/email_reminder/doctype/reminder_settings/reminder_settings.py
@@ -3,60 +3,31 @@
 
 import frappe
 from frappe.model.document import Document
+from frappe.custom.doctype.custom_field.custom_field import create_custom_fields
 
 class ReminderSettings(Document):
-    def generate_fields(self):
-        for doctype in self.doctypes:
-            data = self.get_fields(doctype)
-            for field in data:
-                fieldname = f"{field['dt']}-{(field['label'].lower() if 'label' in field else 'column_break_0').replace(' ', '_')}"
-                cf_check = frappe.db.sql("SELECT * FROM `tabCustom Field` WHERE name=%s", fieldname)
-                if not cf_check:
-                    frappe.get_doc(field).insert(ignore_mandatory=1)
+    def validate(self):
+        self.doctypes = []
+    
+    @frappe.whitelist()
+    def generate_fields(self, docs):
+        if not isinstance(docs,list) or len(docs) == 0:return
+        frappe.db.sql("""UPDATE `tabCustom Field` SET
+                       hidden = 1 where CONCAT(dt, '-', LOWER(dt), '_messages') = name
+                       AND fieldtype='Tab Break'""")
+        frappe.db.commit()
+        for doc in docs :
+            if not doc or frappe.db.exists("Doctype",doc):continue
+            fields = self.get_fields(doc)
+            create_custom_fields(fields)
 
     def get_fields(self, doc):
-        return [
-            {
-                "doctype": "Custom Field",
-                "dt": doc.document_type,
-                "label": "Messages",
-                "fieldtype": "Tab Break"
-            },
-            {
-                "doctype": "Custom Field",
-                "dt": doc.document_type,
-                "insert_after": "messages",
-                "label": "Message",
-                "fieldtype": "Small Text",
-                "allow_on_submit": 1
-            },
-            {
-                "doctype": "Custom Field",
-                "dt": doc.document_type,
-                "insert_after": "message",
-                "label": "Date and Time",
-                "fieldtype": "Datetime",
-                "allow_on_submit": 1
-            },
-            {
-                "doctype": "Custom Field",
-                "dt": doc.document_type,
-                "insert_after": "date_and_time",
-                "fieldtype": "Column Break"
-            },
-            {
-                "doctype": "Custom Field",
-                "dt": doc.document_type,
-                "label": "Emails",
-                "fieldtype": "Table",
-                "options": "Reminder Recipients",
-                "allow_on_submit": 1
-            },
-            {
-                "doctype": "Custom Field",
-                "dt": doc.document_type,
-                "label": "Send Email",
-                "insert_after": "emails",
-                "fieldtype": "Button"
-            }
-        ]
+        dfs = {doc:[
+            frappe._dict(fieldname = f"{doc.lower()}_messages",label = "Messages",fieldtype = "Tab Break", hidden = 0),
+            frappe._dict(fieldname = f"{doc.lower()}_message",label = "Message",fieldtype = "Small Text" , insert_after = f"{doc.lower()}_messages",allow_on_submit = 1),
+            frappe._dict(fieldname = f"{doc.lower()}_date_and_time",label = "Date and Time",fieldtype = "Datetime", insert_after =f"{doc.lower()}_message",allow_on_submit = 1),
+            frappe._dict(fieldname = f"{doc.lower()}_column_break",fieldtype = "Column Break" ,insert_after = f"{doc.lower()}_date_and_time"),
+            frappe._dict(fieldname = f"{doc.lower()}_emails",label = "Emails",fieldtype = "Table", options= "Reminder Recipients",allow_on_submit = 1,insert_after =f"{doc.lower()}_column_break"),
+            frappe._dict(fieldname = f"{doc.lower()}_send_email",label = "Send Email",fieldtype = "Button",insert_after = f"{doc.lower()}_emails"),
+        ]}
+        return dfs
