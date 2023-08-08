7a732acd408cfe50503690ee9d49775c3cd12c5b
husamhammad <husamhammad542@gmail.com>
2023-08-08 09:09:38 +0300

    Refactor: Improve function parameter naming and code structure

--- email_reminder/utils/reminder.py
+++ email_reminder/utils/reminder.py
@@ -10,45 +10,42 @@ def fetch_reminder_doctypes():
 
 @frappe.whitelist()
 def send_email(message, recipients, reminder_schedule_date, doctype, docname, site):
-    if message and recipients and reminder_schedule_date:
-        try:
-            data = json.loads(recipients)
-            emails = [email["email"] for email in data if email]
-            if not emails:
-                frappe.throw(_("Please add at least one email"))
-            
-            doctype_fr = doctype.lower().replace(' ', '-')
-            document_link = f"<a href='{site}/app/{doctype_fr}/{docname}'>Open Document</a>"
-            message = f"""{doctype}<br>" Reminder on Document {docname}<br><br>"{message}<br><br>"
-                    {document_link}"""
-            
-            create_reminder(message, emails, doctype, docname)
-
-            frappe.sendmail(
-                recipients=emails,
-                subject=_("Details"),
-                message=_(message),
-                send_after=reminder_schedule_date,
-            )
-
-            return "Success"
-        
-        except json.JSONDecodeError:
-            frappe.log_error(f"JSON decoding error: {frappe.get_traceback()}")
-        
-        except frappe.exceptions.ValidationError as ve:
-            frappe.log_error(f"Validation Error: {ve}\n{frappe.get_traceback()}")
-
-        except frappe.exceptions.LinkValidationError as lve:
-            frappe.log_error(f"Link Validation Error: {lve}\n{frappe.get_traceback()}")
-
-        except frappe.exceptions.PermissionError as pe:
-            frappe.log_error(f"Permission Error: {pe}\n{frappe.get_traceback()}")
-
-    else :
-        
+    if not( message and recipients and reminder_schedule_date):
         frappe.throw(_("Please Check if All Feilds in Messaging is Full !!"))
 
+    try:
+        data = json.loads(recipients)
+        emails = [email["email"] for email in data if email]
+        if not emails:
+            frappe.throw(_("Please add at least one email"))
+        
+        doctype_fr = doctype.lower().replace(' ', '-')
+        document_link = f"<a href='{site}/app/{doctype_fr}/{docname}'>Open Document</a>"
+        message = f"""{doctype}<br>" Reminder on Document {docname}<br><br>"{message}<br><br>"
+                {document_link}"""
+        
+        create_reminder(message, emails, doctype, docname)
+
+        frappe.sendmail(
+            recipients=emails,
+            subject=_("Details"),
+            message=_(message),
+            send_after=reminder_schedule_date,
+        )
+
+        return "Success"
+    
+    except json.JSONDecodeError:
+        frappe.log_error(f"JSON decoding error: {frappe.get_traceback()}")
+    
+    except frappe.exceptions.ValidationError as ve:
+        frappe.log_error(f"Validation Error: {ve}\n{frappe.get_traceback()}")
+
+    except frappe.exceptions.LinkValidationError as lve:
+        frappe.log_error(f"Link Validation Error: {lve}\n{frappe.get_traceback()}")
+
+    except frappe.exceptions.PermissionError as pe:
+        frappe.log_error(f"Permission Error: {pe}\n{frappe.get_traceback()}")
 
 
 @frappe.whitelist()
