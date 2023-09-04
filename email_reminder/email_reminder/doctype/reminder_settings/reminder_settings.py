8eed7d1ffea849f377f7a59678de22273ad53de5
husamhammad <husamhammad542@gmail.com>
2023-09-04 13:30:41 +0300

    feat: fix the system to working in both version 14 and 13 without an error

--- email_reminder/email_reminder/doctype/reminder_settings/reminder_settings.py
+++ email_reminder/email_reminder/doctype/reminder_settings/reminder_settings.py
@@ -4,7 +4,7 @@
 import frappe
 from frappe.model.document import Document
 from frappe.custom.doctype.custom_field.custom_field import create_custom_fields
-
+from packaging import version
 class ReminderSettings(Document):
     @frappe.whitelist()
     def generate_fields(self, docs):
@@ -33,13 +33,17 @@ class ReminderSettings(Document):
                 custom_field.save()
 
     def get_fields(self, doc):
-        TabBreak = frappe._dict(dt= f"{doc}" ,fieldname = f"{doc.lower()}_messages",label = "Messages",fieldtype = "Tab Break", hidden = 0)
+        frappe_version = frappe.__version__
+        if version.parse(frappe_version) >= version.parse("13.0.0") and version.parse(frappe_version) < version.parse("14.0.0") :
+            Type_Break = frappe._dict(dt=f"{doc}", fieldname=f"{doc.lower()}_messages", label="Messages", fieldtype="Section Break", hidden=0)
+        else:
+            Type_Break = frappe._dict(dt=f"{doc}", fieldname=f"{doc.lower()}_messages", label="Messages", fieldtype="Tab Break", hidden=0)
         Message  = frappe._dict(dt= f"{doc}" ,fieldname = "_message",label = "Message",fieldtype = "Small Text" , insert_after = f"{doc.lower()}_messages",allow_on_submit = 1)
         DateFiled = frappe._dict(dt= f"{doc}" ,fieldname = "rmn_date_and_time",label = "Date and Time",fieldtype = "Datetime", insert_after ="_message",allow_on_submit = 1)
         Column_Break = frappe._dict(dt= f"{doc}" ,fieldname = "rmn_column_break",fieldtype = "Column Break" ,insert_after = "rmn_date_and_time") 
         EmailsField = frappe._dict(dt= f"{doc}" ,fieldname = "rmn_emails",label = "Emails",fieldtype = "Table", options= "Reminder Recipients",allow_on_submit = 1,insert_after ="rmn_column_break")
         SendEmailButton = frappe._dict(dt= f"{doc}" ,fieldname = "send_email",label = "Send Email",fieldtype = "Button",insert_after = "rmn_emails")
         dfs = {doc:[
-            TabBreak,Message,DateFiled,Column_Break,EmailsField,SendEmailButton
+            Type_Break,Message,DateFiled,Column_Break,EmailsField,SendEmailButton
         ]}
         return dfs
